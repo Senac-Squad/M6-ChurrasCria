6fcefd9c663d6ebb6d504e893aa7da62a188ca83
Rodrigo Carlos <87389103+rodrigokcarlos@users.noreply.github.com>
2023-04-03 10:43:28 -0300

    Add files via upload

--- front/src/pages/Avaliar.jsx
+++ front/src/pages/Avaliar.jsx
@@ -1,36 +1,48 @@
-import {Container, Button, Form} from 'react-bootstrap'
-import {useNavigate} from 'react-router-dom'
-import axios from 'axios'
-import '../styles/corrigibts.css'
-function Avaliar() {
-  const navigate = useNavigate();
+import Container from 'react-bootstrap/Container'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import Api from '../api/api.js'
+import '../styles/Login.css';
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    alert('Obrigado pelo contato')
-    navigate('/home');
-  }
 
-    return (
-        <Container className='conteudo-margin'>
-            <h1>Contato</h1>
-    
+const Login = () => {
+  
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    const form = event.currentTarget
+    const nota = form.elements.Nota.value
+    const mensagem = form.elements.Mensagem.value
 
-            <Form onSubmit={handleSubmit}>
-                <Form.Group className="mb-3" controlId="Nota">
-                    <Form.Label>Nome</Form.Label>
-                    <Form.Control type="text" placeholder="Digite seu nome completo"  />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="Mensagem">
-                    <Form.Label>Mensagem</Form.Label>
-                    <Form.Control type="text" placeholder="Digite sua mensagem" />
-                </Form.Group>
-                <Button variant="primary" type='submit' onClick={(e) => handleSubmit(e)}>
-                    Enviar contato
-                </Button>
-            </Form>
+    const retorno = await Api().postAvaliar({nota, mensagem})
+    if(!retorno.ok){
+        alert('Não foi possível mandar uma avaliação.')
+        return
+    }
 
-        </Container>
-    );
+    window.location.href = '/home'
 }
-export default Avaliar;
+
+return (
+    <Container className='conteudo-margin'>
+    <h1>Avaliação</h1>
+
+    <Form onSubmit={handleSubmit}>
+            <Form.Group className="" controlId="Nota">
+                <Form.Label>Nota</Form.Label>
+                <Form.Control type="number" placeholder="Digite uma Nota de 1 a 5" maxLength={1}/>
+            </Form.Group>
+            <Form.Group className="" controlId="Mensagem">
+                <Form.Label>Mensagem</Form.Label>
+                <Form.Control type="text" placeholder="Deixe uma mensagem!" />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+                Realizar Avaliação
+            </Button  >
+        </Form>
+
+    </Container>
+)
+}
+
+export default Login;
